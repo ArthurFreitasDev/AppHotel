646f2283804bc6dc27ce943ef5afb929c2ae1b15
Arthur Cesar de Freitas <102485949+Arthur6002@users.noreply.github.com>
2023-07-31 12:29:39 -0300

    Update App.xaml.cs

--- AppHotel/AppHotel/App.xaml.cs
+++ AppHotel/AppHotel/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using AppHotel.Model;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using System.Collections.Generic;
@@ -10,11 +11,40 @@ namespace AppHotel
 {
     public partial class App : Application
     {
+        public List<Suite> Lista_Suites = new List<Suite>()
+        {
+            new Suite()
+            {
+                Nome = "Super Luxo",
+                DiariaAdulto = 110.0,
+                DiariaCrianca = 55.0
+            },
+            new Suite()
+            {
+                Nome = "Executiva",
+                DiariaAdulto = 90.0,
+                DiariaCrianca = 45.0
+            },
+            new Suite()
+            {
+                Nome = "Crise",
+                DiariaAdulto = 45.0,
+                DiariaCrianca = 20.0
+            }
+        };
+
         public App()
         {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
+
             InitializeComponent();
 
-            MainPage = new MainPage();
+            //MainPage = new NavigationPage(new View.ComtratacaoHospedagem());
+
+            if (Properties.ContainsKey("Usuario_Logado"))
+                MainPage = new NavigationPage(new View.ComtratacaoHospedagem());
+            else
+                MainPage = new NavigationPage(new View.Login());
         }
 
         protected override void OnStart()
